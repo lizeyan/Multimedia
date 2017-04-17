a4927db3526afc55b51056cbe4cc594ecdbed8ef
lizeyan <zy-li14@hotmail.com>
2017-04-17 16:11:56 +0800

    Image codec

--- image_codec.py
+++ image_codec.py
@@ -58,28 +58,49 @@ def psnr(arr_a, arr_b, max_possible=255):
 
 
 def image_codec_main():
+    output_path = "./output/image_codec"
+    clean_folder(output_path)
     lena = Image.open("lena.bmp")
     gray_scale_lena = gray_scale(lena)
-    gray_scale_lena.save("./output/gray_scale_lena.bmp")
-    # test dct1d
+    gray_scale_lena.save(os.path.join(output_path, "gray_scale_lena.bmp"))
+    w, h = gray_scale_lena.size
+    gray_scale_lena_size = w * h
+    selectors = [zig_zag_selector(gray_scale_lena_size / (4 ** i), h, w) for i in range(4)]
+    for idx, selector in enumerate(selectors):
+        test_dct1d(selector, "1/%d" % (4 ** idx), gray_scale_lena, output_path)
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+
+    for idx, selector in enumerate(selectors):
+        test_dct2d(selector, "1/%d" % (4 ** idx), gray_scale_lena, output_path)
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+
+    selectors = [zig_zag_selector(64 / (4 ** i), 8, 8) for i in range(4)]
+    for idx, selector in enumerate(selectors):
+        test_dct2d(selector, "1/%d" % (4 ** idx), gray_scale_lena, output_path, (8, 8))
+
+
+def test_dct1d(selector, selector_indicator, image, output_path):
     print("=============================================")
-    dct1d_cft = dct1d_codec(np.asarray(gray_scale_lena))
-    Image.fromarray(dct1d_cft.astype(np.int8), "L").save("./output/dct1d_cft_lena.bmp")
+    dct1d_cft = dct1d_codec(np.asarray(image))
+    dct1d_cft *= selector
+    Image.fromarray(dct1d_cft.astype(np.int8), "L").save(os.path.join(output_path, "dct1d_cft_lena.bmp"))
     idct1d_lena = Image.fromarray(idct1d_codec(dct1d_cft).astype(np.int8), "L")
-    idct1d_lena.save("./output/idct1d_lena.bmp")
-    print("PSNR of 1D DCT codec: %fdB" % psnr(np.asarray(gray_scale_lena), np.asarray(idct1d_lena)))
-    # test dct2d
+    idct1d_lena.save(os.path.join(output_path, "idct1d_lena_%s.bmp") % selector_indicator.replace("/", "_"))
+    print("PSNR of 1D DCT codec: %fdB" % psnr(np.asarray(image), np.asarray(idct1d_lena)), ", use %s DCT coefficients" % selector_indicator)
 
-    def test_dct2d(block_size=(0, 0)):
-        print("=============================================")
-        dct2d_cft = dct2d_codec(np.asarray(gray_scale_lena), block_size)
-        Image.fromarray(dct2d_cft.astype(np.int8), "L").save("./output/dct2d_cft_lena_%d%d.bmp" % block_size)
-        idct2d_lena = Image.fromarray(idct2d_codec(dct2d_cft, block_size).astype(np.int8), "L")
-        idct2d_lena.save("./output/idct2d_lena_%d%d.bmp" % block_size)
-        print("PSNR of 2D DCT codec: %fdB, block size: %d, %d" % (psnr(np.asarray(gray_scale_lena), np.asarray(idct2d_lena)), block_size[0], block_size[1]))
 
-    test_dct2d()
-    test_dct2d((8, 8))
+def test_dct2d(selector, selector_indicator, image, output_path, block_size=(0, 0)):
+    print("=============================================")
+    if block_size[0] == 0 or block_size[1] == 0:
+        block_size = image.size
+    dct2d_cft = dct2d_codec(np.asarray(image), block_size)
+    dct2d_cft_blocks = blockwise(dct2d_cft, block_size)
+    dct2d_cft_blocks *= np.expand_dims(np.expand_dims(selector, 0), 0)
+    dct2d_cft = block_join(dct2d_cft_blocks)
+    Image.fromarray(dct2d_cft.astype(np.int8), "L").save(os.path.join(output_path, "dct2d_cft_lena_%d_%d.bmp") % block_size)
+    idct2d_lena = Image.fromarray(idct2d_codec(dct2d_cft, block_size).astype(np.int8), "L")
+    idct2d_lena.save(os.path.join(output_path, "idct2d_lena_%d_%d_%s.bmp") % (block_size[0], block_size[1], selector_indicator.replace("/", "_")))
+    print("PSNR of 2D DCT codec: %fdB, block size: %d, %d" % (psnr(np.asarray(image), np.asarray(idct2d_lena)), block_size[0], block_size[1]), ", use %s DCT coefficients" % selector_indicator)
 
 
 if __name__ == '__main__':
--- utility.py
+++ utility.py
@@ -1,12 +1,24 @@
+import os
+
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
 import time
+from shutil import *
 
 
 def blockwise(matrix, block=(3, 3)):
     shape = (int(matrix.shape[0] / block[0]), int(matrix.shape[1] / block[1])) + block
     strides = (matrix.strides[0] * block[0], matrix.strides[1] * block[1]) + matrix.strides
     return as_strided(matrix, shape=shape, strides=strides)
+
+
+def clean_folder(path):
+    if os.path.exists(path):
+        assert os.path.isdir(path)
+        rmtree(path)
+        os.mkdir(path)
+    else:
+        os.makedirs(path)
 
 
 def block_join(blocks):
@@ -31,5 +43,49 @@ def profile(func):
     return wrap
 
 
+def zig_zag_selector(length, rows, columns):
+    assert rows > 0 and columns > 0 and length > 0
+    if length >= rows * columns:
+        return np.ones(shape=(rows, columns), dtype=int)
+    ret = np.zeros(shape=(rows, columns), dtype=int)
+    last = np.asarray((0, 0))
+    cnt = 0
+    adder_tuple = [(-1, 1), (1, -1)]
+    adder = adder_tuple[1]
+    line_cnt = 0
+    length = min(length, rows * columns)
+    line_start = False
+    while True:
+        ret[last[0], last[1]] = 1
+        cnt += 1
+        if cnt >= length:
+            break
+        if not line_start and (last[0] == 0 or last[1] == 0 or last[1] == columns - 1 or last[0] == rows - 1):
+            if line_cnt % 2 == 0:
+                last = last + (1, 0) if last[1] == columns - 1 else last + (0, 1)
+            else:
+                last = last + (0, 1) if last[0] == rows - 1 else last + (1, 0)
+
+            line_start = True
+            line_cnt += 1
+            adder = adder_tuple[line_cnt % 2]
+        else:
+            line_start = False
+            last += adder
+
+    return ret
+
+
+def _test_zig_zag_selector():
+    # print(zig_zag_selector(5, 8, 8))
+    # print(zig_zag_selector(56, 8, 8))
+    print(zig_zag_selector(5, 3, 3))
+    print(zig_zag_selector(6, 3, 3))
+    print(zig_zag_selector(7, 3, 3))
+    print(zig_zag_selector(8, 3, 3))
+    print(zig_zag_selector(65, 3, 3))
+
+
 if __name__ == '__main__':
-    _test_block()
+    # _test_block()
+    _test_zig_zag_selector()
